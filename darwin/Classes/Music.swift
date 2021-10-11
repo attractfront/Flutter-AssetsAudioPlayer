c21ef07239665503167d1a3ec8302aa8c6027463
Viktoriia <viktoriia.mk@attractgroup.com>
2021-10-11 15:45:14 +0300

    add logs

--- darwin/Classes/Music.swift
+++ darwin/Classes/Music.swift
@@ -526,6 +526,11 @@ public class Player : NSObject, AVAudioPlayerDelegate {
                     
                 }
             }
+            
+            print("play music")
+            print(AVAudioSession.sharedInstance().category)
+            print(AVAudioSession.sharedInstance().mode)
+            print(AVAudioSession.sharedInstance().categoryOptions)
             #endif
             
             var item : SlowMoPlayerItem
